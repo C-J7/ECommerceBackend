7dae31996f7e8f1d560a903f4c6e44457c3f5856
C-J7 <bamgbosechristian@gmail.com>
2025-10-28 15:36:34 +0100

    updated drizzle config file.

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
-  schema: "./src/db/schema",
+  schema: "./dist/src/db/schema",
   dbCredentials: {
     url: process.env.NEON_DB_CONNECTION_STRING || "Database connection URL not found",
   },
